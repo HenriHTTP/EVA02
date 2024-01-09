e0fa7ff0bc0fa4e0046b3c2e9da34d772a118752
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-09 00:11:31 -0300

    refactor: fix messages_send_on_server

--- commands/greetings.py
+++ commands/greetings.py
@@ -12,7 +12,7 @@ class Greetings(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        messages_send_on_server =
+        messages_send_on_server = f'message from  {message.author} content: {message.content}'
         print(messages_send_on_server)
 
     @commands.Cog.listener()
