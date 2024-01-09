f038a4af61c2f22afb96602532c6de7be9160f0f
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-09 06:39:42 -0300

    refactor: remove import, rename message and use message.Message

--- commands/greetings.py
+++ commands/greetings.py
@@ -1,5 +1,5 @@
-import discord
 from discord.ext import commands
+from utils import message
 
 
 class Greetings(commands.Cog):
@@ -7,19 +7,19 @@ class Greetings(commands.Cog):
         self.__bot = bot
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        messages_send_on_server = f'message from  {message.author} content: {message.content}'
+    async def on_message(self, ctx):
+        messages_send_on_server = f'message from  {ctx.author} content: {ctx.content}'
         print(messages_send_on_server)
 
     @commands.Cog.listener()
     async def on_member_join(self, ctx: commands.Context):
         on_member_join_message = f"Welcome to the server, {ctx.author.mention} We're glad to have you here"
-        await ctx.author.send(on_member_join_message)
+        await message.Message.send_message_user(ctx, on_member_join_message)
 
     @commands.command(name='hello')
     async def greet_user(self, ctx: commands.Context):
         greet_user_message = f'hello, {ctx.author} how are you my friend, be welcome to {ctx.guild}'
-        await ctx.reply(greet_user_message)
+        await message.Message.reply_message_user(ctx, greet_user_message)
 
 
 async def setup(bot: commands.bot):
