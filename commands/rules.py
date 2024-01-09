9dd3d324d9b8301036f17e53e0d2529fda577f05
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-09 06:40:13 -0300

    feat: create rules

---
+++ commands/rules.py
@@ -0,0 +1,21 @@
+from discord.ext import commands
+from utils import message
+
+
+class Rules(commands.Cog):
+    def __init__(self, bot: commands.bot):
+        self.__bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        await message.Message.remove_messages(ctx)
+        await self.__reply_to_mention(ctx)
+
+    async def __reply_to_mention(self, ctx):
+        message_reply = f"hello, {ctx.author.mention}"
+        await message.Message.reply_mention_message(self.__bot, ctx, message_reply)
+
+
+async def setup(bot: commands.bot):
+    await bot.add_cog(Rules(bot))
+
