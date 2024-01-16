a54ce61eed2ba9ef1c872598ef8b994552deafb3
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-15 23:53:52 -0300

    feat: add open_ai

---
+++ utils/open_ia.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+import os
+import openai
+from discord.ext import commands
+
+load_dotenv()
+OPEN_IA_TOKEN = os.getenv('OPEN_IA_TOKEN')
+
+openai.api_key = OPEN_IA_TOKEN
+
+
+class OpenAi:
+
+    @staticmethod
+    async def ask_gpt(messages):
+        try:
+            response = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo-1',
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": messages},
+                ],
+                temperature=0.7,
+                max_tokens=150
+            )
+            return response['choices'][0]['message']['content']
+        except Exception as e:
+            print(f"Error asking GPT-3: {e}")
+            return "An error occurred while processing your request."
+
+    @staticmethod
+    async def reply_messages(ctx: commands.Context):
+        print(ctx.message.content)
+        pass
