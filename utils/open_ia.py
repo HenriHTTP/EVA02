1af5aadd6d9332350ff84347e9219ed685118973
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-17 14:52:43 -0300

    refactor: rename e to error

--- utils/open_ia.py
+++ utils/open_ia.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
 import openai
-from discord.ext import commands
 
 load_dotenv()
 OPEN_IA_TOKEN = os.getenv('OPEN_IA_TOKEN')
@@ -24,6 +23,6 @@ class OpenAi:
                 max_tokens=150
             )
             return response['choices'][0]['message']['content']
-        except Exception as e:
-            print(f"Error asking GPT-3: {e}")
+        except Exception as error:
+            print(f"Error asking GPT-3: {error}")
             return "An error occurred while processing your request."
