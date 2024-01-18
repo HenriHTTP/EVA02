25c221f02ac97bad79ea5d600c9730d20674fe59
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-18 20:21:45 -0300

    feat: use new message method

--- commands/rules.py
+++ commands/rules.py
@@ -11,11 +11,14 @@ class Rules(commands.Cog):
         await Message.remove_messages(ctx)
         await self.__reply_to_mention(ctx)
 
-    async def __reply_to_mention(self, ctx):
-        message_reply = f"hello, {ctx.author.mention}"
-        await Message.reply_mention_message(self.__bot, ctx, message_reply)
+    async def __reply_to_mention(self, ctx: commands.Context):
+        try:
+            message_reply = f"hello, {ctx.author.name}"
+            message = Message(ctx, message_reply, self.__bot)
+            await message.reply_mention_message()
+        except Exception as error:
+            print(error)
 
 
 async def setup(bot: commands.bot):
     await bot.add_cog(Rules(bot))
-
