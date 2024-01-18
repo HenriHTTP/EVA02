e6f0c578fe774b5179eeb2a38787cf5711362776
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-18 20:21:03 -0300

    refactor: static method to method

--- utils/message.py
+++ utils/message.py
@@ -1,34 +1,44 @@
 from discord.ext import commands
+from views.greetings_view import GreetingView
 
 
 class Message:
-    @staticmethod
-    async def reply_message_user(ctx: commands.Context, message: str):
-        try:
-            await ctx.reply(message)
-        except Exception as error:
-            print(error)
-            return False
-
-    @staticmethod
-    async def send_message_user(ctx: commands.Context, message: str):
-        try:
-            await ctx.send(message)
-        except Exception as error:
-            print(error)
-            return False
+    def __init__(self, ctx: commands.Context, message: str, bot: commands.Bot):
+        self.ctx = ctx
+        self.message = message
+        self.__bot = bot
 
     @staticmethod
     async def remove_messages(message):
         if "words" in message.content.lower():
             await message.delete()
 
-    @staticmethod
-    async def reply_mention_message(bot: commands.Bot, ctx: any, message: str):
-        if bot.user.mentioned_in(ctx):
-            mention_message = message
+    async def send_message_user(self):
+        try:
+            view = GreetingView(self.ctx, self.message, self.__bot)
+            message = await self.ctx.send(embed=view.to_embed(), ephemeral=True)
+            view.message = message
+            return True
+        except Exception as error:
+            print(error)
+            return False
+
+    async def reply_message_user(self):
+        try:
+            view = GreetingView(self.ctx, self.message, self.__bot)
+            message = await self.ctx.reply(embed=view.to_embed())
+            view.message = message
+            return True
+        except Exception as error:
+            print(error)
+            return False
+
+    async def reply_mention_message(self):
+        if self.__bot.user.mentioned_in(self.ctx):
             try:
-                await ctx.channel.send(mention_message)
+                view = GreetingView(self.ctx, self.message, self.__bot)
+                message = await self.ctx.reply(embed=view.to_embed())
+                view.message = message
                 return True
             except Exception as error:
                 print(error)
