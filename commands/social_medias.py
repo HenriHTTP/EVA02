f364c17dbe8bea7ff999e921c5bb7cce782195a5
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-18 20:23:10 -0300

    refactor: remove spaces

--- commands/social_medias.py
+++ commands/social_medias.py
@@ -5,13 +5,12 @@ from discord.ext import commands
 
 class Social(commands.Cog):
     def __init__(self, bot: commands.Bot):
-        self.__bot = bot 
-        
-    @commands.command(name='Amazon') 
-    async def Amazon (self,ctx: commands.Context, *search):
+        self.__bot = bot
+
+    @commands.command(name='Amazon')
+    async def amazon(self, ctx: commands.Context, *search):
         pass
-    
-    
+
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(Social(bot))    
+    await bot.add_cog(Social(bot))
