fd0ab7ef8f9a620eae1fe02c1953e1f9361c3ead
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-18 20:23:30 -0300

    refactor: use new method kitsu

--- commands/animes.py
+++ commands/animes.py
@@ -2,16 +2,20 @@ from discord.ext import commands
 from discord.ext.commands import bot
 from utils.kitsu import Kitsu
 from utils.message import Message
+from discord.ui import Button, view
 
 
 class Anime(commands.Cog):
-    def __init__(self, bot: commands.bot):
+    def __init__(self, bot: commands.Bot):
         self.__bot = bot
 
     @commands.command(name="anime")
     async def search_anime(self, ctx: commands.Context, *search: str):
-        await Kitsu.get_anime(ctx,*search)
+        print(*search)
+        anime = Kitsu(ctx, self.__bot, *search)
+        await anime.get_anime()
+        print(self.__bot.user.name)
 
 
-async def setup(bot: commands):
+async def setup(bot: commands.Bot):
     await bot.add_cog(Anime(bot))
