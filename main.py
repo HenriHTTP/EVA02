346707e0db594e2ff439707e40da3643eca3cbdd
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-19 18:26:52 -0300

    feat: remove client start, main and use new prefix

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import os
 import asyncio
 from dotenv import load_dotenv
 
+
 load_dotenv()
 DISCORD_TOKEN = os.getenv('TOKEN')
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 
 async def load_cogs():
@@ -15,9 +16,5 @@ async def load_cogs():
             await client.load_extension("commands." + packages[:-3])
 
 
-async def main():
-    await load_cogs()
-    await client.start(DISCORD_TOKEN)
-
-
-asyncio.run(main())
+asyncio.run(load_cogs())
+client.run(DISCORD_TOKEN)
