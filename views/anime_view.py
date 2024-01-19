8ca1eb3bd05e02d40d1f592664ef224d257983bf
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-19 18:14:12 -0300

    refactor: use private members

--- views/anime_view.py
+++ views/anime_view.py
@@ -5,20 +5,20 @@ from discord.ui import View
 class AnimeView(View):
     def __init__(self, ctx, title, average_rating, synopsis, poster_image):
         super().__init__()
-        self.ctx = ctx
-        self.title = title
-        self.average_rating = average_rating
-        self.synopsis = synopsis
-        self.poster_image = poster_image
+        self.__ctx = ctx
+        self.__title = title
+        self.__average_rating = average_rating
+        self.__synopsis = synopsis
+        self.__poster_image = poster_image
 
     async def interaction_check(self, interaction: discord.Interaction):
-        return interaction.user == self.ctx.author
+        return interaction.user == self.__ctx.author
 
     def to_embed(self):
         embed = discord.Embed(
-            title=self.title,
-            description=f"Average Rating: {self.average_rating}\nSynopsis: {self.synopsis}",
+            title=self.__title,
+            description=f"Average Rating: {self.__average_rating}\n Synopsis: {self.__synopsis}",
             color=discord.Color.blue()
         )
-        embed.set_thumbnail(url=self.poster_image)
+        embed.set_thumbnail(url=self.__poster_image)
         return embed
--- views/greetings_view.py
+++ views/greetings_view.py
@@ -6,18 +6,18 @@ from discord.ext import commands
 class GreetingView(View):
     def __init__(self, ctx: commands.Context, message: str, bot: commands.Bot):
         super().__init__()
-        self.ctx = ctx
-        self.message = message
-        self.bot = bot
+        self.__ctx = ctx
+        self.__message = message
+        self.__bot = bot
 
     async def interaction_check(self, interaction: discord.Interaction):
-        return interaction.user == self.ctx.author
+        return interaction.user == self.__ctx.author
 
     def to_embed(self):
         embed = discord.Embed(
-            title=self.bot.user.name,
-            description=f"{self.message}",
+            title=self.__bot.user.name,
+            description=f"{self.__message}",
             color=discord.Color.blue()
         )
-        embed.set_thumbnail(url=self.bot.user.avatar.url)
+        embed.set_thumbnail(url=self.__bot.user.avatar.url)
         return embed
