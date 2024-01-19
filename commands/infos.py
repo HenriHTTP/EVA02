c24ea11616ffbad497929eaec050cb4a15639c68
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-19 18:24:01 -0300

    refactor: add Github profile

--- commands/infos.py
+++ commands/infos.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 
 class Info(commands.Cog):
-    def __init__(self, bot: commands.bot):
+    def __init__(self, bot: Bot):
         self.__bot = bot
 
     @commands.Cog.listener()
@@ -20,13 +21,18 @@ class Info(commands.Cog):
         return self.__bot.user.name
 
     async def __print_bot_info(self):
-        print('Message: connected to Discord as Bot', await self.__get_bot_name())
-        print('Message: Bot', await self.__get_bot_name(), 'connected on', await self.__get_all_server_count(),
-              'servers')
+        print('###############################################')
+        print('#           Template made by HenriHTTP        #')
+        print('#          https://github.com/HenriHTTP       #')
+        print('#           Copyright© HenriHTTP, 2024        #')
+        print('#           Do Not Remove This Header         #')
+        print('###############################################\n')
+        print(f'message: connected to Discord as Bot{await self.__get_bot_name()}')
+        print(f'Message: Bot {await self.__get_bot_name()}connected on {await self.__get_all_server_count()} servers')
         discord_names = await self.__get_discord_server()
         for names in discord_names:
             print('serve:', names)
 
 
-async def setup(bot: commands.Bot):
+async def setup(bot: Bot):
     await bot.add_cog(Info(bot))
