5d5400c44ac8c183d986cf6155dd403e8d5637f2
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-19 18:22:53 -0300

    refactor: use private members

--- utils/channel.py
+++ utils/channel.py
@@ -4,12 +4,12 @@ from utils.message import Message
 
 class Channel:
     def __init__(self, ctx, bot):
-        self.ctx = ctx
+        self.__ctx = ctx
         self.__bot = bot
 
     async def connect_to_channel(self):
         try:
-            voice_channel = self.ctx.author.voice.channel
+            voice_channel = self.__ctx.author.voice.channel
             if voice_channel:
                 await voice_channel.connect()
                 return True
