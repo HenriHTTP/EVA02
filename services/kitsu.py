d8f9a6b578c262041f46792824dbd62dc7730f69
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-22 13:44:00 -0300

    refactor: implement disnake, ephemeral, __no_anime method

--- services/kitsu.py
+++ services/kitsu.py
@@ -1,37 +1,44 @@
+###############################################
+#           Template made by HenriHTTP        #
+#          https://github.com/HenriHTTP       #
+#           Copyright© HenriHTTP, 2024        #
+###############################################
+
 import requests
-from discord.ext import commands
-from utils.message import Message
 from views.anime_view import AnimeView
-from discord.ext.commands import Context, Bot
 
 
 class Kitsu:
-    def __init__(self, ctx: Context, bot: Bot, *anime: str):
+    def __init__(self, ctx, bot, anime: str):
         self.__ctx = ctx
         self.__bot = bot
         self.__anime = anime
 
     async def get_anime(self):
+        print(self.__anime)
+        await self.__no_anime()
         base_url = 'https://kitsu.io/api/edge/'
-        search_query = ' '.join(self.__anime)
-        url_search = f'{base_url}anime?filter[text]={search_query}'
+        url_search = f'{base_url}anime?filter[text]={self.__anime}'
         response = requests.get(url_search)
         response_is_complete = response.status_code == 200
-        if response_is_complete:
-            data = response.json()
-            animes = data.get('data', [])
-            if response:
-                for anime in animes:
-                    attributes = anime.get('attributes', {})
-                    canonical_title = attributes.get('canonicalTitle', 'N/A')
-                    average_rating = attributes.get('averageRating', 'N/A')
-                    synopsis = attributes.get('synopsis', 'N/A')
-                    poster_image = attributes.get('posterImage', {}).get('original', 'N/A')
-                    await self.__send_anime_message(canonical_title, average_rating, synopsis, poster_image)
-            else:
-                await self.__no_anime_message()
+        try:
+            if response_is_complete:
+                data = response.json()
+                animes = data.get('data', [])
+                if response:
+                    for anime in animes:
+                        attributes = anime.get('attributes', {})
+                        canonical_title = attributes.get('canonicalTitle', 'N/A')
+                        average_rating = attributes.get('averageRating', 'N/A')
+                        synopsis = attributes.get('synopsis', 'N/A')
+                        poster_image = attributes.get('posterImage', {}).get('original', 'N/A')
+                        await self.__send_anime_message(canonical_title, average_rating, synopsis, poster_image)
+                else:
+                    await self.__ctx.send('No anime found')
+        except Exception as error:
+            print(error)
         else:
-            print(f"Error:{response.status_code}")
+            print(f"Request Error:{response.status_code}")
 
     async def __send_anime_message(self, canonical_title, average_rating, synopsis, poster_image):
         anime_view = AnimeView(
@@ -41,10 +48,11 @@ class Kitsu:
             synopsis=synopsis,
             poster_image=poster_image
         )
-        message = await self.__ctx.send(embed=anime_view.to_embed(), ephemeral=True)
+        embed = anime_view.to_embed()
+        message = await self.__ctx.send(embed=embed, ephemeral=True)
         anime_view.message = message
 
-    async def __no_anime_message(self):
-        message_error = "No anime"
-        message = Message(self.__ctx, message_error, self.__bot)
-        await message.reply_message_user()
+    async def __no_anime(self):
+        if not self.__anime:
+            await self.__ctx.send("No anime")
+            return False
