ba72aa250f57bb1fbc5416422c06224d137751b2
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-22 13:44:39 -0300

    refactor: implement disnake, add members param

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -1,10 +1,16 @@
-from discord.ext import commands
+###############################################
+#           Template made by HenriHTTP        #
+#          https://github.com/HenriHTTP       #
+#           Copyright© HenriHTTP, 2024        #
+###############################################
+
 from utils.message import Message
-from discord.ext.commands import Context, Bot
+from disnake.ext import commands
+from disnake.ext.commands import Bot
 
 
 class Greetings(commands.Cog):
-    def __init__(self, bot: Bot):
+    def __init__(self, bot):
         self.__bot = bot
 
     @commands.Cog.listener()
@@ -13,17 +19,17 @@ class Greetings(commands.Cog):
         print(messages_send_on_server)
 
     @commands.Cog.listener()
-    async def on_member_join(self, ctx: Context):
+    async def on_member_join(self, ctx):
         on_member_join_message = f"Welcome to the server, {ctx.author.mention} We're glad to have you here"
         message = Message(ctx, on_member_join_message, self.__bot)
         await message.send_message_user()
 
-    @commands.hybrid_command(name='hello', description='greetings to user.')
-    async def greet_user(self, ctx: Context):
+    @commands.slash_command(name="hello", description='greetings to user.')
+    async def greet_user(self, ctx, member: str = None):
         greet_user_message = f'hello, {ctx.author} how are you my friend, be welcome to {ctx.guild}'
         message = Message(ctx, greet_user_message, self.__bot)
         await message.send_message_user()
 
 
-async def setup(bot: Bot):
-    await bot.add_cog(Greetings(bot))
+def setup(bot):
+    bot.add_cog(Greetings(bot))
