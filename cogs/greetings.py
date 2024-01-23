14f42e905388e84cf0c4d2aacb718398b8482a01
Henrique da Silva <86864451+HenriHTTP@users.noreply.github.com>
2024-01-23 09:08:59 -0300

    feat: fix send message on member join

--- cogs/greetings.py
+++ cogs/greetings.py
@@ -19,13 +19,12 @@ class Greetings(commands.Cog):
         print(messages_send_on_server)
 
     @commands.Cog.listener()
-    async def on_member_join(self, ctx):
-        on_member_join_message = f"Welcome to the server, {ctx.author.mention} We're glad to have you here"
-        message = Message(ctx, on_member_join_message, self.__bot)
-        await message.send_message_user()
+    async def on_member_join(self, member):
+        on_member_join_message = f"Welcome to the server, {member.name} We're glad to have you here"
+        await member.send(on_member_join_message)
 
     @commands.slash_command(name="hello", description='greetings to user.')
-    async def greet_user(self, ctx, member: str = None):
+    async def greet_user(self, ctx):
         greet_user_message = f'hello, {ctx.author} how are you my friend, be welcome to {ctx.guild}'
         message = Message(ctx, greet_user_message, self.__bot)
         await message.send_message_user()
